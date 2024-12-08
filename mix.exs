13344f0c961c26f6e911e4f4da7edac8d9f307a8
Anaceli Barbosa de Oliveira <anacelibarbosa@gmail.com>
2024-12-08 20:35:43 -0300

    Add decimal lib to deps

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule ExBanking.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:decimal, "~> 2.0"}
     ]
   end
 
